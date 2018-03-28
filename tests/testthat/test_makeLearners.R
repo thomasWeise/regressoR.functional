6a3257d5e54c4004a986bc8abf7d297434629024
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-28 11:36:14 +0800

    Slow Tests Reduced for Travis

--- tests/testthat/test_makeLearners.R
+++ tests/testthat/test_makeLearners.R
@@ -1,6 +1,8 @@
 library("regressoR.functional")
 context("FunctionalModel.makeLearners")
 
+slow.tests <- (is.na(Sys.getenv("TRAVIS", unset=NA)))
+
 test_that("Test FunctionalModel.makeLearners", {
   learners <- FunctionalModel.makeLearners();
   expect_true(!is.null(learners));
@@ -13,12 +15,16 @@ test_that("Test FunctionalModel.makeLearners", {
   y <- func(x);
 
   metric <- regressoR.quality::RegressionQualityMetric.default(x, y);
+  full.tests <- TRUE;
 
   for(learner in learners) {
     expect_true(is.function(learner));
-    result <- learner(metric, NULL, NULL, NULL);
-    expect_true(!is.null(result));
-    expect_is(result, "FittedFunctionalModel");
-    validObject(result);
+    if(full.tests) {
+      result <- learner(metric, NULL, NULL, NULL);
+      expect_true(!is.null(result));
+      expect_is(result, "FittedFunctionalModel");
+      validObject(result);
+    }
+    full.tests <- full.tests && slow.tests;
   }
 })
